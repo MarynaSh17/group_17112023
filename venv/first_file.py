283bccb04014d4874b5ec29b4d2ff857d7a2c85a
Maryna Shovkova <marinka685m16@gmail.com>
2024-01-09 18:54:10 +0000

    add first file

--- venv/first_file.py
+++ venv/first_file.py
@@ -1,3 +1,3 @@
-favorite_city = "Kyiv is the capital and most populous city of Ukraine. It is in north-central Ukraine along the Dnieper River."
+favorite_city = "Kyiv is the capital and most populous city of Ukraine.It is in north-central Ukraine along the Dnieper River."
 print(favorite_city)
 
